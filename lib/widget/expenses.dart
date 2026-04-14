ee7f0ab0335d53b74828973d4bd2f413a58af2bb
Vincent-zhangS <vzhang2022@gmail.com>
2026-04-14 11:44:31 -0400

    made snackbar with undo feature

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -26,9 +26,24 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 3),
+        content: Text('Expense Deleted!'),
+        action: SnackBarAction(
+          label: 'Undo', 
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+        }),
+      ),
+    );
   }
 
   final List<Expense> _registeredExpenses = [
@@ -53,6 +68,15 @@ class _ExpensesState extends State<Expenses> {
   ];
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No Expense. Click + to add one!'),
+    );
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: _removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -67,10 +91,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           Text('CHART GOES HERE'),
           Expanded(
-            child: ExpensesList(
-              onRemoveExpense: _removeExpense,
-              expenses: _registeredExpenses
-            ),
+            child: mainContent
           ),
         ],
       ),
